85faca1e2edfd9b267f04355181cccba4174a754
Esteban Donaire Martínez <edonmar@gmail.com>
2021-04-16 13:38:59 +0200

    Añadido efecto a WallAura

--- Assets/Scripts/WallAura.cs
+++ Assets/Scripts/WallAura.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,88 @@ public class WallAura : MonoBehaviour
     private GameManager manager;
 
     public Dictionary<string, int> elements; // Después de eliminar SHI, EAR e ICE
+    private Dictionary<string, int> dmgTypes;
+    private bool canHit;
+    private bool isExplosive;
 
     private void Start()
     {
         manager = GameObject.Find("Manager").GetComponent<GameManager>();
+
+        isExplosive = GetIsExplosive();
+        if (isExplosive)
+            return;
+        dmgTypes = GetDamageTypesDictionary();
+        StartCoroutine(HitTimer(0.25f));
+    }
+
+    private bool GetIsExplosive()
+    {
+        return elements.ContainsKey("LIF") || elements.ContainsKey("ARC");
+    }
+
+    private Dictionary<string, int> GetDamageTypesDictionary()
+    {
+        Dictionary<string, int> dmgTypesDict = new Dictionary<string, int>();
+
+        int waterCount = 0;
+        int coldCount = 0;
+        int lightningCount = 0;
+        int fireCount = 0;
+        int steamCount = 0;
+
+        if (elements.ContainsKey("WAT"))
+            waterCount = elements["WAT"];
+        if (elements.ContainsKey("COL"))
+            coldCount = elements["COL"];
+        if (elements.ContainsKey("LIG"))
+            lightningCount = elements["LIG"];
+        if (elements.ContainsKey("FIR"))
+            fireCount = elements["FIR"];
+        if (elements.ContainsKey("STE"))
+            steamCount = elements["STE"];
+
+        if (waterCount > 0)
+            dmgTypesDict.Add("WAT", 282 + 52 * waterCount);
+        if (coldCount > 0)
+            dmgTypesDict.Add("COL", 25 + 5 * coldCount);
+        if (lightningCount > 0)
+            dmgTypesDict.Add("LIG", 282 + 52 * lightningCount);
+        if (fireCount > 0)
+            dmgTypesDict.Add("FIR", 120 + 30 * fireCount);
+        if (steamCount > 0)
+            dmgTypesDict.Add("STE", 280 + 51 * steamCount);
+
+        return dmgTypesDict;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        CheckCharacterHit(other);
+    }
+
+    private void CheckCharacterHit(Collider other)
+    {
+        if (!canHit)
+            return;
+
+        string otherTag = other.tag;
+        if (otherTag != "Player" && otherTag != "Enemy")
+            return;
+
+        CharacterStats characterStats = other.GetComponent<CharacterStats>();
+        if (characterStats.health != 0)
+            characterStats.TakeSpell(dmgTypes);
+        canHit = false;
+    }
+
+    private IEnumerator HitTimer(float hitRate)
+    {
+        while (true)
+        {
+            canHit = true;
+            yield return new WaitForSeconds(hitRate);
+        }
     }
 
     public void DestroyThis()
