a631f9c97c0192dbf5c9ed58974c8479bebac1cb
Esteban Donaire Martínez <edonmar@gmail.com>
2021-05-22 10:51:41 +0200

    Añadidas animaciones a los enemigos
    Importadas animaciones de idle, movimiento, morir, atacar, ser golpeado.
    Implementadas la de morir y la de movimiento a Skeleton y a Golem. La velocidad de reproducción de la animación de movimiento depende de la velocidad de movimiento.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,18 +3,23 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private CharacterStats characterStats;
     [SerializeField] private NavMeshAgent navMeshAgent;
 
     private bool isNavMeshAgentEnabled;
     private Transform player;
     private CharacterStats playerCharacterStats;
+    
+    private int hashParamMovSpeed;
 
     private void Start()
     {
         isNavMeshAgentEnabled = true;
         player = GameObject.Find("Player").transform;
         playerCharacterStats = player.gameObject.GetComponent<CharacterStats>();
+        
+        hashParamMovSpeed = Animator.StringToHash("MovSpeed");
     }
 
     private void Update()
@@ -27,6 +32,9 @@ public class Enemy : MonoBehaviour
 
         if (isNavMeshAgentEnabled && playerCharacterStats.health > 0)
             navMeshAgent.destination = player.position;
+        
+        float movSpeed = characterStats.movSpeed;
+        animator.SetFloat(hashParamMovSpeed, movSpeed);
     }
 
     public void EnableNavMeshAgent()
